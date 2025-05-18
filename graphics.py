b1e84c4179e57b2e7bbf7276baf3bf9c636914f8
Ermin Osmanovic <ermin.osmanovic@gmail.com>
2025-05-18 12:33:21 +0300

    Cell class done

--- graphics.py
+++ graphics.py
@@ -3,11 +3,12 @@ from tkinter import Tk, BOTH, Canvas
 class Window: 
     def __init__(self, width, height):
         self.__root=Tk()
-        self.__root.title("My window")
+        self.__root.title("Maze Solver")
         self.__root.geometry(f"{width}x{height}")  
         self.__canvas = Canvas(self.__root, width=width, height=height)
-        self.__canvas.pack
+        self.__canvas.pack()
         self.window_running=False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
         self.__root.update()
@@ -20,6 +21,30 @@ class Window:
 
     def close(self):
         self.window_running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        
+
+    def draw_line(self, line, fill_color="black"):
+        
+        line.draw(self.__canvas, fill_color)
 
 
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color="red"):
+        
+        canvas.create_line(
+            self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
+            )
+
+
+  
+
+        
--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-from graphics import Window
+from graphics import Window, Point, Line
+from cell import Cell
 
 def main():
     win = Window(800, 600)
+
+    # Draw a line
+    p1 = Point(35, 50)
+    p2 = Point(120, 200)
+    line = Line(p1, p2)
+    win.draw_line(line, "red")
+
+    
+
+    # Draw cells
+    # Draw cells
+    cell1 = Cell(win)
+    cell1.draw(100, 150, 100, 150)
+
+    cell2 = Cell(win)
+    cell2.draw(160, 210, 100, 150)
+
+    cell3 = Cell(win)
+    cell3.draw(100, 150, 160, 210)
+
+    cell4 = Cell(win)
+    cell4.draw(160, 210, 160, 210)
+
+    # Draw move from cell1 to cell2
+    cell1.draw_move(cell2, undo=False)
+
+
     win.wait_for_close()
+
 
 main()
